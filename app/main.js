1676e7c8031926fed72aa22f71761160fd2289a6
Ashley <grigsby.ashley@gmail.com>
2017-10-09 13:46:57 -0400

    the start of something awesome

---
+++ app/main.js
@@ -0,0 +1,35 @@
+var contactList = new Vue({
+
+
+	el: '#contactList',
+
+	data: {
+		contacts: [
+		{
+			id: 1,
+			name:'Albus Dumbledore'
+			phonenumber : '606-234-9876'
+		},
+		{
+			id: 2,
+			name: 'Minerva McGonagall'
+			phonenumber : '606-234-9876'
+		},
+		{
+			id: 3,
+			name: 'Serverus Snape'
+			phonenumber : '502-444-1010'
+		}
+
+
+		]
+
+	},
+
+
+	methods: {
+
+
+
+	}
+});
---
+++ docs/requirements.md
@@ -0,0 +1,20 @@
+# Contact List
+
+We need to make a contact list so we don't forget any og out contacts we met inLongson.
+
+## Technology
+
+1. Vue
+2. Vue Components
+3. Bootstrap 4
+
+## Requirements
+
+1. Title: Contact List
+2. Horizontal line
+3. Name input
+4. Phone input
+5. Save button
+6. Another horz line
+7.
+8.
